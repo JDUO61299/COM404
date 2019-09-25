092b9c3cce7cb68cef6084ac62ff9b2468b1520c
JDUO61299 <4duoj51@solent.ac.uk>
2019-09-25 10:55:56 +0000

    added code to read user input

--- 0-setup/main.py
+++ 0-setup/main.py
@@ -1,9 +1,10 @@
 
 
-name = "Jordan"
-age = 12
+name = input("please enter a name")
+age = int(input("please enter an age")
 height = 1.8
 initial = 'J'
 isAwesome = True
-
-print("Hello, " + name + " you are " + str(age) + " years old ")
+age = age+1
+print("Hello, " + name + " you are " + int(age) + " years old ")
+print("yay "*10)
