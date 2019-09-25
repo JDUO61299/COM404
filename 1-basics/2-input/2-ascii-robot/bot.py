cbe5b5e5ca58faecfe84ff4e5268e2172728bed5
JDUO61299 <4duoj51@solent.ac.uk>
2019-09-25 11:13:47 +0000

    added code to display an ascii robot with dynamic eyes

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,26 @@
+# ask user for preferred eye type
+eyes = input("please enter a character to represent the eyes")
+
+
+
+
+
+
+
+print("############")
+print("# "+eyes+"     "+eyes+"  #")
+print("#          #")
+print("#   ----   #")
+print("############")
+print("    |  |   ")
+print("    |  |    ")
+print(" -----------")
+print(" |          |")
+print(" |          |")
+print(" |          |")
+print(" |          |")
+print(" ------------")
+print("   | |   | |")
+print("   | |   | |")
+print("   | |   | |")
+print("  ----- -----")
