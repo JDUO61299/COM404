dbd3946eec474e38a9d85f6dd19827e5582c7c70
JDUO61299 <4duoj51@solent.ac.uk>
2019-10-02 11:15:38 +0000

    Added code to demonstrate the use of a logical and operator

---
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,15 @@
+sound = input("what did i hear")
+if sound == "hoot":
+    print("what did i see?")
+    hoot = input()
+    if hoot == "some big eyes":
+        print("i am a little scared but i will continue")
+
+elif sound == "grrr":
+    print("what did i see?")
+    grrr = input()
+    if grrr == "two red eyes":
+        print("there is a scary creature, i need to get out of here!")
+
+else:
+    print("I am a little scared but I will continue")
