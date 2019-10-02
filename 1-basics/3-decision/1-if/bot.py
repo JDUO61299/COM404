c1d61c524dd2aa8a3ab6ba9ca51b7fcff55e381c
JDUO61299 <4duoj51@solent.ac.uk>
2019-10-02 08:57:34 +0000

    Added code to demonstrate the use of an if...else statement

--- 1-basics/3-decision/1-if/bot.py
+++ 1-basics/3-decision/1-if/bot.py
@@ -3,3 +3,12 @@ if book == "adventure":
     print("I like adventure books!")
 
 print("finished reading book")
+
+
+activity = (input("Please enter the activity to be performed"))
+if activity == "calculate":
+    print("performing calculations...")
+elif activity != "calculate":
+    print("performing activity...")
+
+print("activity completed!")
